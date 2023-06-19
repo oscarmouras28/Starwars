3f4757cb9aafacd006285e00493ef3d1659e7aa6
oscarmouras28 <oscar.mourasp@gmail.com>
2023-06-19 14:43:09 -0400

    pulida pero no al 100%, faltante el diseño, imagenes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
